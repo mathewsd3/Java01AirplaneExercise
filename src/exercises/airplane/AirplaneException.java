7f7c40388baec9d828b981d74967dffae50096c8
mathewsd3 <mathewsdawn3@gmail.com>
2020-01-25 14:38:32 -0500

    first commit

---
+++ src/exercises/airplane/AirplaneException.java
@@ -0,0 +1,11 @@
+package exercises.airplane;
+
+public class AirplaneException extends Exception //checked
+{
+	//make our own exceptions
+	public AirplaneException(String message)
+	{
+		super(message);
+	}
+
+}
